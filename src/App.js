32da206962081691af159f759a134a4553b8519e
Safa-M <mohamedsafa61@gmail.com>
2021-09-24 15:18:43 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -7,16 +7,18 @@ function App() {
     <>
       <div className="App">
         <br />
-          <Data FullName="Safa Mohamed"
+        <Data
+          FullName="Safa Mohamed"
           Profession="Joined Go My Code as a Full-Stack JS student"
           Bio="Safa is a 25 years old electrical and automation engineer who graduated from 
           the National Engineering School Of Gabes ENIG, which is endowed with robotics and embedded systems,
           with a humbled knowledge in industrial diagnosis and maintenance."
-          HandleName={HandleName}>
-            <img src={"/IMG.jpg"} alt=""></img>
-          </Data>
-          <br />
-          <br />
+          HandleName={HandleName}
+        >
+          <img src={"/IMG.jpg"} alt="" width="10px"></img>
+        </Data>
+        <br />
+        <br />
       </div>
     </>
   );
